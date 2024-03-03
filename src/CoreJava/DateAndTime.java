24f793bb00ebcd34bc0ae92aabed8f7c290464c3
Mona Lisa <vikram1.pogula@gmail.com>
2024-03-03 19:22:30 +0530

    branch added

--- src/CoreJava/DateAndTime.java
+++ src/CoreJava/DateAndTime.java
@@ -8,5 +8,6 @@ public class DateAndTime {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(df.format(now));
+        System.out.println(df.format(now));
     }
 }
