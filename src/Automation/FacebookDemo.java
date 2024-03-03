7c52af1b3ed7b78fa96bfcc809d5f7c8f9526ab0
Mona Lisa <vikram1.pogula@gmail.com>
2024-03-03 19:15:17 +0530

    automation added

--- src/Automation/FacebookDemo.java
+++ src/Automation/FacebookDemo.java
@@ -3,7 +3,6 @@ package Automation;
 public class FacebookDemo {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "E:\\VIKPROJECT\\chro\\chromedriver.exe");
-         WebDriver driver = new ChromeDriver();
 
     }
 }
---
+++ src/CoreJava/DateAndTime.java
@@ -0,0 +1,12 @@
+package CoreJava;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(df.format(now));
+    }
+}
