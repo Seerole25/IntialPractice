0fe2ac0d13e055d9d85a498e3355ef61c3965920
Mona Lisa <vikram1.pogula@gmail.com>
2024-03-03 18:07:48 +0530

    automation

--- src/Automation/Demo.java
+++
@@ -1,4 +0,0 @@
-package Automation;
-
-public class Demo {
-}
---
+++ src/Automation/FacebookDemo.java
@@ -0,0 +1,9 @@
+package Automation;
+
+public class FacebookDemo {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "E:\\VIKPROJECT\\chro\\chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
+
+    }
+}
