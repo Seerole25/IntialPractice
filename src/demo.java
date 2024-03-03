f342bce327447c2ce6cff15ac8685ff1594ad00d
Mona Lisa <vikram1.pogula@gmail.com>
2024-03-03 16:04:08 +0530

    First Commit

--- src/demo.java
+++ src/demo.java
@@ -1,7 +1,12 @@
 public class demo {
-    public static void main(String[] args){
-        System.out.println("Demo Project");
-        System.out.println("Demo Project");
+    public static void main(String[] args) {
 
+        int a = 10;
+
+        if (a < 11) {
+            System.out.print("The given statement is correct");
+        } else {
+            System.out.print("The given statement is in correct");
+        }
     }
 }
